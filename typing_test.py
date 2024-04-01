a0b088cc4369b46f89c7f2ee860869f3192fa069
hannah-felize <hannah.felize@gmail.com>
2024-04-01 16:10:41 -0700

    capture individual characters

--- typing_test.py
+++ typing_test.py
@@ -1,5 +1,6 @@
 import sqlite3
 import time
+import msvcrt
 
 # Connect to the database
 conn = sqlite3.connect('typing_test.db')
@@ -25,25 +26,29 @@ def conduct_typing_test(user):
     start_time = time.time()
     previous_key = None
 
-    while True:
-        key = input()
+    typing_data = []  # Array to store typing test data
 
-        if key == "":
+    while True:
+        key = msvcrt.getche()
+
+        if key == b'\r':  # Enter key
             break
 
         current_time = time.time()
         time_between_presses = current_time - start_time
 
-        # Save the typing test data to the database
-        cursor.execute('''
-            INSERT INTO typing_data (user, key_pressed, previous_key_pressed, time_between_presses)
-            VALUES (?, ?, ?, ?)
-        ''', (user, key, previous_key, time_between_presses))
-        conn.commit()
+        # Save the typing test data to the array
+        typing_data.append((user, key, previous_key, time_between_presses))
 
         previous_key = key
         start_time = current_time
 
+    # Insert the typing test data into the database
+    cursor.executemany('''
+        INSERT INTO typing_data (user, key_pressed, previous_key_pressed, time_between_presses)
+        VALUES (?, ?, ?, ?)
+    ''', typing_data)
+    conn.commit()
     print("Typing test completed. Thank you!")
 
 # Usage example
