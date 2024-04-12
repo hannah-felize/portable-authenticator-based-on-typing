8b9684068a675bc8838ead6d4525aee4b46748ba
hannah-felize <hannah.felize@gmail.com>
2024-04-12 15:50:02 -0700

    data fields separated in preprocessed sqlite

--- rf_classifier.py
+++ rf_classifier.py
@@ -24,23 +24,45 @@ for typing_test in typing_data_df:
         count_key_presses += 1
         if count_key_presses > max_key_presses:
             max_key_presses = count_key_presses
+    count_key_presses = 0
 print(max_key_presses)
 
 # Create a new sqlite3 table to store the preprocessed typing data
-# cursor = conn.cursor()
+cursor = conn.cursor()
+table_columns = ['id INTEGER PRIMARY KEY AUTOINCREMENT', 'user TEXT']
+for i in range(1, max_key_presses+1):
+    table_columns.append(f'key_pressed_{i} TEXT')
+    table_columns.append(f'previous_key_pressed_{i} TEXT')
+    table_columns.append(f'time_between_presses_{i} REAL')
 
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS preprocessed_typing_data (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         user TEXT,
-#         key_pressed TEXT,
-#         previous_key_pressed TEXT,
-#         time_between_presses REAL
-#     )
-# ''')
+table_columns_str = ', '.join(table_columns)
 
+cursor.execute(f'''
+    CREATE TABLE IF NOT EXISTS preprocessed_typing_data (
+        {table_columns_str}
+    )
+''')
 
+# Copy all data from the 'typing_data' table to the 'preprocessed_typing_data' table
+for index, row in df.iterrows():
+    user = row['user']
+    typing_data = json.loads(row['typing_data'])
+    typing_data_dict = {'user': user}
+    for i, key_press in enumerate(typing_data):
+        typing_data_dict[f'key_pressed_{i+1}'] = key_press['key_pressed']
+        typing_data_dict[f'previous_key_pressed_{i+1}'] = key_press['previous_key_pressed']
+        typing_data_dict[f'time_between_presses_{i+1}'] = key_press['time_between_presses']
 
+    # Insert the preprocessed typing data into the new table
+    columns = ', '.join(typing_data_dict.keys())
+    placeholders = ', '.join(['?' for _ in typing_data_dict.values()])
+    values = list(typing_data_dict.values())
+
+    cursor.execute(f'''
+        INSERT INTO preprocessed_typing_data ({columns})
+        VALUES ({placeholders})
+    ''', values)
+    conn.commit()
 
 # # Features are all columns except 'user'
 # X = df.drop('user', axis=1)
